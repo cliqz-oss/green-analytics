4984bcab103e858dd134db7ad6af26c20f19f527
Sam Macbeth <sam@cliqz.com>
2017-06-01 16:39:31 +0200

    Add messages table and collector

--- web/app.py
+++ web/app.py
@@ -1,9 +1,15 @@
 from index import app, db
 from controllers.sites import Sites
+from controllers.messages import Messages
 
 @app.route('/sites', methods=['GET', 'POST'])
 def sites():
     controller = Sites()
+    return controller.respond()
+
+@app.route('/collect', methods=['POST'])
+def collect():
+    controller = Messages()
     return controller.respond()
 
 @app.route('/', methods=['GET'])
