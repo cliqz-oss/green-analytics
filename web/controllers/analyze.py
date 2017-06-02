4ce56ffba743150582c0e172d08a9c860db942d7
Sam Macbeth <sam@cliqz.com>
2017-06-02 16:36:50 +0200

    Basic dashboard

--- web/controllers/analyze.py
+++ web/controllers/analyze.py
@@ -1,11 +1,20 @@
 from flask import request, render_template
-from report import analyze_log
+from datetime import datetime, timedelta
+from controllers.metric_detail import MetricData
 
 class Analyze():
 
     def respond(self):
-    	LOG_FILE = './logs/collect.jl.bkp1'
-    	LOGS_DICT = {}
-    	context = analyze_log(LOG_FILE)
-    	context['site1'] = 'site1.test.cliqz.com'
+        metrics = MetricData()
+        stop = datetime.utcnow()
+        start = stop - timedelta(30)
+        token = request.args.get('token')
+        sites = metrics.report_domains({'token': token})
+        context = {
+            'from': start.strftime('%Y/%m/%d %H:%Mh'),
+            'to': stop.strftime('%Y/%m/%d %H:%Mh'),
+            'token': token,
+            'sites': sites,
+            'selected_site': request.args.get('site', sites[0][0])
+        }
         return render_template('dashboard.html', context=context)
